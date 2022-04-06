9409312758656281d14eefb378ad92764691c631
Harley <36212178+HarleyCheema@users.noreply.github.com>
2022-04-06 12:55:29 -0500

    Update UserIO.java

--- src/main/java/dvdlibrary/ui/UserIO.java
+++ src/main/java/dvdlibrary/ui/UserIO.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * This is the interface for the UserIO Console implementation. 
  */
 package dvdlibrary.ui;
 
