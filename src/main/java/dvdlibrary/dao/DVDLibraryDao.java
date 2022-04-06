d0b08bf3156c56442da076cc1ce3261c210e02c0
SavageNode <99512977+SavageNode@users.noreply.github.com>
2022-04-06 10:47:04 -0500

    Update DVDLibraryDao.java

--- src/main/java/dvdlibrary/dao/DVDLibraryDao.java
+++ src/main/java/dvdlibrary/dao/DVDLibraryDao.java
@@ -4,10 +4,53 @@
  */
 package dvdlibrary.dao;
 
+import java.util.List;
+import DVDLibrary.dto.DVD;
 /**
  *
  * @author harle
  */
+//Dejan Savic
 public interface DVDLibraryDao {
-    
+    /**
+     * Adds the given DVD to the library and associates it with the given
+     * title. If there is already a dvd associated with the given
+     * DVD title it will return that DVD object, otherwise it will
+     * return null.
+     *
+     * @param DVD title with which DVD is to be associated
+     * @param DVD dvd to be added to the Library
+     * @return the DVD object previously associated with the given  
+     * DVD title if it exists, null otherwise
+     */
+    DVD addDVD(String title, DVD dvd);
+
+    /**
+     * Returns a List of all DVDs in the library.
+     *
+     * @return List containing all dvds in the library.
+     */
+    List<DVD> getAllDVDs();
+
+    /**
+     * Returns the DVD object associated with the given DVD title.
+     * Returns null if no such DVD exists
+     *
+     * @param DVD title of the DVD to retrieve
+     * @return the DVD object associated with the given DVD title,  
+     * null if no such DVD exists
+     */
+    DVD getDVD(String title);
+
+    /**
+     * Removes from the library the DVD associated with the given title.
+     * Returns the DVD object that is being removed or null if
+     * there is no DVD associated with the given title
+     *
+     * @param dvdTitle tile of DVD to be removed
+     * @return DVD object that was removed or null if DVD
+     * was associated with the given student title
+     */
+    DVD removeDVD(String title);
 }
+
